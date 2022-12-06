8fda1e6ab7aa347fff2b258dacd552a26c37f2c4
adityaparasar <119102374+adityaparasar@users.noreply.github.com>
2022-12-06 11:15:38 +0530

    Add files via upload

---
+++ BInarStringProblem.java
@@ -0,0 +1,16 @@
+public class BInarStringProblem {
+    public static void binaryString(int n,char lastdigit,String str){
+        if(n==0){
+            System.out.println(str);
+            return;
+        }
+        binaryString(n-1, '0', str+'0');
+        if(lastdigit=='0'){
+            binaryString(n-1, '1', str+'1');
+        }
+       }
+    
+       public static void main(String args[]){
+        binaryString(3, '0', "");
+       }
+}
