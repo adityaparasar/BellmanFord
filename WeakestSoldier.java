5da99395ab4e1f7c4fbc13a7eba11ef66a56dcaf
Aditya Parasar <119102374+adityaparasar@users.noreply.github.com>
2023-03-25 14:29:38 +0530

    Add files via upload
    We are given an mxn binary matrix of 1's (soldiers) and O's (civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the O's in each row.
    A row i is weaker than a row j if one of the following is true:
    • The number of soldiers in row i is less than the number of soldiers in row j.
    • Both rows have the same number of soldiers and i<j. Find the K weakest rows.
    m=4, n=4, k=2
    1000
    1111
    1000
    1000

---
+++ WeakestSoldier.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class WeakestSoldier {
+    static class Row implements Comparable<Row>{
+        int soldier;
+        int idx;
+        public Row(int soldier, int idx){
+            this.soldier=soldier;
+            this.idx= idx;
+        }
+        @Override
+        public int compareTo(Row r2){
+            if(this.soldier==r2.soldier){
+                return this.idx-r2.idx;
+            }else{
+                return this.soldier-r2.soldier;
+            }
+        }
+    }
+    
+    public static void main (String args[]){
+        int army[][]={{1,0,0,0},
+                      {1,1,1,1},
+                      {1,0,0,0},
+                      {1,0,0,0}};
+        int k=2;
+
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+        for(int i=0; i<army.length; i++){
+            int soldier=0;
+            for(int j=0; j<army[0].length; j++){
+                if(army[i][j]==1){
+                    soldier+=1;
+                }
+            }
+            pq.add(new Row(soldier, i));
+        }
+
+        for(int i=0; i<k;i++){
+            System.out.print("Row"+pq.remove().idx+" ");
+        }
+    }
+}
