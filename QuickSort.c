4bc752ce78461752905964d9ee8182a5df18bd2c
Aditya Parasar <119102374+adityaparasar@users.noreply.github.com>
2023-05-25 00:04:55 +0530

    QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -1,55 +1,36 @@
 #include<stdio.h>
-#include<stdio.h>
-int max, min;
-int a[100];
-void maxmin(int i, int j)
-{
- int max1, min1, mid;
- if(i==j)
- {
-  max = min = a[i];
- }
- else
- {
-  if(i == j-1)
-  {
-   if(a[i] <a[j])
-   {
-    max = a[j];
-    min = a[i];
+void quicksort(int num[25],int f,int l){
+   int i, j, p, temp;
+   if(f<l){
+      p=f;i=f;j=l;
+      while(i<j){
+         while(num[i]<=num[p]&&i<l)
+         i++;
+         while(num[j]>num[p])
+         j--;
+         if(i<j){
+            temp=num[i];
+            num[i]=num[j];
+            num[j]=temp;
+         }
+      }
+      temp=num[p];
+      num[p]=num[j];
+      num[j]=temp;
+      quicksort(num,f,j-1);
+      quicksort(num,j+1,l);
    }
-   else
-   {
-    max = a[i];
-    min = a[j];
-   }
-  }
-  else
-  {
-   mid = (i+j)/2;
-   maxmin(i, mid);
-   max1 = max; min1 = min;
-   maxmin(mid+1, j);
-   if(max <max1)
-    max = max1;
-   if(min > min1)
-    min = min1;
-  }
- }
 }
-int main ()
-{
- int i, num;
- printf ("\nEnter the total number of numbers : ");
- scanf ("%d",&num);
- printf ("Enter the numbers : \n");
- for (i=1;i<=num;i++)
-  scanf ("%d",&a[i]);
-
- max = a[0];
- min = a[0];
- maxmin(1, num);
- printf ("Minimum element in an array : %d\n", min);
- printf ("Maximum element in an array : %d\n", max);
- return 0;
+int main(){
+   int i, count, num[25];
+   printf("How many elements are u going to enter?: ");
+   scanf("%d",&count);
+   printf("Enter %d elements: ", count);
+   for(i=0;i<count;i++)
+   scanf("%d",&num[i]);
+   quicksort(num,0,count-1);
+   printf("Order of Sorted elements: ");
+   for(i=0;i<count;i++)
+   printf(" %d",num[i]);
+   return 0;
 }
